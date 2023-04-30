03441e71314c0077073e0c04abe0be1267b8cac6
crizzhd1 <djralph1@hotmail.com>
2023-04-30 19:41:01 -0400

    boom effects

--- src/main/java/com/crizzhd/mod/mixin/CreeperEnityMixin.java
+++ src/main/java/com/crizzhd/mod/mixin/CreeperEnityMixin.java
@@ -1,17 +1,24 @@
 package com.crizzhd.mod.mixin;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.CreeperEntity;
-import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import java.util.Random;
 import static com.crizzhd.mod.block.ModBlocks.Crizz_Block;
 
 @Mixin(CreeperEntity.class)
-public abstract class CreeperEnityMixin extends HostileEntity {
+public abstract class CreeperEnityMixin extends LivingEntity {
 
-    protected CreeperEnityMixin(EntityType<? extends HostileEntity> entityType, World world) {
+    protected CreeperEnityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
 
@@ -20,15 +27,38 @@ public abstract class CreeperEnityMixin extends HostileEntity {
         super.setMovementSpeed(movementSpeed);
     }
 
-    @Override
     public void onDeath(DamageSource damageSource) {
         dropItem(Crizz_Block, 0);
     }
 
-    public float GetRandomFloat(float min, float max) {
+    public float GetRandomFloat(float min, float max){
         Random ran = new Random();
         float random = ran.nextFloat(max);
         random = random + min;
         return random;
+    }
+
+    public StatusEffect RandonEffect() {
+
+        StatusEffect effects = null;
+        int i = random.nextInt(5);
+
+        if (i <= 1) {
+            effects = StatusEffects.GLOWING;
+        } else if (i <= 2) {
+            effects = StatusEffects.BAD_OMEN;
+        } else if (i <= 3) {
+            effects = StatusEffects.JUMP_BOOST;
+        } else if (i <= 4) {
+            effects = StatusEffects.LEVITATION;
+        }
+        return effects;
+    }
+
+    @Inject(method = "explode", at = @At("HEAD"))
+    private void applyEffect(CallbackInfo ci) {
+        StatusEffectInstance effectInstance;
+        effectInstance = new StatusEffectInstance(RandonEffect(), (int) GetRandomFloat(100,400));
+        ((LivingEntity)(Object)this).addStatusEffect(effectInstance);
     }
 }
